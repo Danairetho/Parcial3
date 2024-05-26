05604e47a8d758f69ed1266d70a2919491ebfa3c
Danairetho <danairetho@uninorte.edu.co>
2024-05-26 15:29:20 -0500

    Creación de clases para las operaciones y la interfaz de la operación

---
+++ src/core/Interfaz/InterfaceOp.java
@@ -0,0 +1,16 @@
+package core.Interfaz;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author danao
+ */
+public interface InterfaceOp {
+    
+    public abstract double resultado (double number1, double number2);
+    
+}
---
+++ src/core/Operaciones/Addition.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.Operaciones;
+
+import core.Interfaz.InterfaceOp;
+
+/**
+ *
+ * @author danao
+ */
+public class Addition implements InterfaceOp{
+
+    public Addition() {
+        
+    }
+
+    @Override
+    public double resultado(double number1, double number2) {
+        return number1 + number2;
+    }
+    
+}
